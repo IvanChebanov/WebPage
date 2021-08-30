ba4d0aac8e7ab5aed922196d64a8b8a0af1a5417
IvanChebanov <64684680+IvanChebanov@users.noreply.github.com>
2021-08-30 10:33:44 +0300

    a
    a

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-function playBGM() {
-  document.getElementById("sdfsdfasda");
-}
+
+const musicBtn = document.querySelector('.music-btn');
+const musicLine = document.querySelector('.bgm-line');
+const audioPlayer = document.querySelector('audio');
+musicBtn.onclick = function() {
+	if(musicLine.style.visibility === "hidden"){
+		musicLine.style.visibility = "visible";
+		audioPlayer.pause();	
+	}else{
+		musicLine.style.visibility = "hidden";
+		audioPlayer.play();
+	}
+};
+console.log(musicBtn);
+console.log(musicLine);
+console.log(audioPlayer);
+
+
+/* TEST SCRIPT */
