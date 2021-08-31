445c05ee2fc97a5d01a66c266ca488d0afb1a033
IvanChebanov <64684680+IvanChebanov@users.noreply.github.com>
2021-08-31 15:58:12 +0300

    vsCodeTest

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,11 @@ const musicBtn = document.querySelector('.music-btn');
 const musicLine = document.querySelector('.bgm-line');
 const audioPlayer = document.querySelector('audio');
 
-musicBtn.onclick = function() {
-	if(musicLine.hidden){
+musicBtn.onclick = function () {
+	if (musicLine.hidden) {
 		musicLine.hidden = false;
-		audioPlayer.pause();	
-	}else{
+		audioPlayer.pause();
+	} else {
 		musicLine.hidden = true;
 		audioPlayer.play();
 	}
