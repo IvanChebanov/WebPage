bd08ec9ae3a026fb1b81f332bb99bdb5365f0f8a
IvanChebanov <64684680+IvanChebanov@users.noreply.github.com>
2021-09-25 00:09:06 +0300

    piano

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,11 @@
-const musicBtn = document.querySelector('.music-btn');
-const musicLine = document.querySelector('.bgm-line');
-const audioPlayer = document.querySelector('audio');
+//BGM BUTTON
+const musicBtn = document.querySelector('.header__bgm-btn');
+const musicLine = document.querySelector('.line');
+const audioPlayer = document.querySelector('.header__bgm-audio');
+//
+const keys = document.querySelectorAll('.keys');
 
+//BGM BUTTON
 musicBtn.onclick = function () {
 	if (musicLine.hidden) {
 		musicLine.hidden = false;
@@ -11,6 +15,13 @@ musicBtn.onclick = function () {
 		audioPlayer.play();
 	}
 };
-// console.log(musicBtn);
-// console.log(musicLine);
-// console.log(audioPlayer);
+//
+
+keys.forEach(key => {
+	key.addEventListener('click', () => playNote(key))
+});
+
+function playNote(key){
+	const noteAudio = document.getElementById(key.dataset.note);
+	noteAudio.play();
+}
